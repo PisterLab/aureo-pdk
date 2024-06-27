94c9f9aa3789852caa116af667fa932943c05d45
Alex5958 <liuyichen1234@outlook.com>
2024-06-27 16:05:41 -0700

    Adding NIMPLANT, PIMPLANT, and BOUNDARY layer to tf file

--- aureo.tf
+++ aureo.tf
@@ -56,6 +56,9 @@ layerDefinitions(
   ( POLY2                     16         P2           )
   ( SOI2                      17         S2           )
   ( SOIHOLE2                  18         S2HOLE       )
+  ( PIMPLANT                  19         PIMP         )
+  ( NIMPLANT                  20         NIMP         )
+  ( BOUNDARY                  43         BOUND        )
  ) ;techLayers
 
  techDerivedLayers(
@@ -79,6 +82,9 @@ layerDefinitions(
   ( POLY2                     drawing    )
   ( LOWMETAL2                 drawing    )
   ( HIGHMETAL2                drawing    )
+  ( PIMPLANT                  drawing    )
+  ( NIMPLANT                  drawing    )
+  ( BOUNDARY                  drawing    )
   ( POLY1                     net        )
   ( POLY2                     net        )
   ( LOWMETAL1                 net        )
@@ -117,6 +123,8 @@ layerDefinitions(
   ( LOWMETAL2    drawing      blueSolid        t t t t t )
   ( POLY2        drawing      orangeSolidDe    t t t t t )
   ( SOI2         drawing      magentaSolidSp   t t t t t )
+  ( PIMPLANT     drawing      purpleSolid    t t t t t )
+  ( NIMPLANT     drawing      tanSolid  t t t t t )
 
   ( POLY1        net          yellowDashed     t t t t nil )
   ( POLY2        net          blueDashed       t t t t nil )
@@ -155,8 +163,10 @@ techLayerProperties(
   ( sheetResistance       HIGHMETAL2              0.0244  )
   ( sheetResistance       SOI1                    2.5     )
   ( sheetResistance       SOI2                    2.5     )
-  ( sheetResistance       POLY1                   30     )
-  ( sheetResistance       POLY2                   30     )
+  ( sheetResistance       POLY1                   30      )
+  ( sheetResistance       POLY2                   30      )
+  ( sheetResistance       PIMPLANT                2.5     )
+  ( sheetResistance       NIMPLANT                2.5     )
 )
 
 
@@ -171,7 +181,7 @@ layerRules(
   functions(
     ;( layer          function         [maskNumber] )
     ;( -----          --------          ----------  )
-    ( SOI1          "li"                1          )
+    ( SOI1          "substrate"         1          )
     ( LOWMETAL1     "li"                3          )
     ( POLY1         "li"                4          )
     ( HIGHMETAL1    "li"                5          )
@@ -179,6 +189,8 @@ layerRules(
     ( POLY2         "li"                8          )
     ( LOWMETAL2     "li"                9          )
     ( SOI2          "li"                10         )
+    ( NIMPLANT      "li"                11         )
+    ( PIMPLANT      "li"                12         )
     
     
   ) ;functions
